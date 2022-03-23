a6582179475f245dbc4c55aeffd350f407dc349d
r4920 <hr_105@yopmail.com>
2022-03-23 10:54:19 +0000

    Code Synced by DhiWise

---
+++ lib/core/app_export.dart
@@ -0,0 +1,28 @@
+export 'package:get/get.dart';
+
+export 'package:rahul_s_application3/localization/app_localization.dart';
+
+export 'package:rahul_s_application3/core/constants/constants.dart';
+
+export 'package:rahul_s_application3/core/utils/image_constant.dart';
+export 'package:rahul_s_application3/core/utils/color_constant.dart';
+
+export 'package:rahul_s_application3/core/utils/math_utils.dart';
+export 'package:rahul_s_application3/core/utils/pref_utils.dart';
+
+export 'package:rahul_s_application3/core/utils/initial_bindings.dart';
+
+export 'package:rahul_s_application3/theme/app_style.dart';
+export 'package:rahul_s_application3/theme/app_decoration.dart';
+
+export 'package:connectivity_plus/connectivity_plus.dart';
+
+export 'package:rahul_s_application3/routes/app_routes.dart';
+
+export 'package:rahul_s_application3/data/models/selectionPopupModel/selection_popup_model.dart';
+
+export 'package:rahul_s_application3/core/widgets/common_network_image.dart';
+export 'package:rahul_s_application3/core/widgets/paginated_list_view.dart';
+
+export 'package:rahul_s_application3/core/errors/exceptions.dart';
+export 'package:rahul_s_application3/core/network/network_info.dart';
